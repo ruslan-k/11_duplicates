260fd91c66a67e14aba8fa9875520d0d18157f16
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-12-30 16:24:41 +0300

    Simplified find_dublicates function

--- duplicates.py
+++ duplicates.py
@@ -3,32 +3,16 @@ import sys
 
 
 def find_duplicates(dir_path):
-    # list of dicts for files (name, size, path)
-    files_dict = []
-    # set of pairs 'file - filesize'
-    files_set = set()
+    files_dict = {}
     for path, dirs, files in os.walk(dir_path):
         for file in files:
             full_path = os.path.join(path, file)
             filesize = os.path.getsize(full_path)
-            file_dict = {}
-            file_dict["name"] = file
-            file_dict['size'] = filesize
-            file_dict['path'] = full_path
-            files_dict.append(file_dict)
-            files_set.add((file, filesize))
-
-    # dict with full paths to file for pair 'name - filesize'
-    dupes = {}
-    for name, size in files_set:
-        for item in files_dict:
-            if item['name'] == name and item['size'] == size:
-                if dupes.get((name, size)):
-                    dupes[(name, size)].append(item['path'])
-                else:
-                    dupes[(name, size)] = [item['path']]
-
-    dupes_filter = dict((file, paths) for file, paths in dupes.items() if len(paths) > 1)
+            if not (file, filesize) in files_dict:
+                files_dict[(file, filesize)] = [full_path]
+            else:
+                files_dict[(file, filesize)].append(full_path)
+    dupes_filter = dict((file, paths) for file, paths in files_dict.items() if len(paths) > 1)
     return dupes_filter
 
 def list_file_dublicates(dir_path):
@@ -43,7 +27,6 @@ def list_file_dublicates(dir_path):
 
 if __name__ == '__main__':
     try:
-        print(list_file_dublicates(sys.argv[1]))
+        list_file_dublicates(sys.argv[1])
     except IndexError:
         print('Не указан путь к файлу')
-
